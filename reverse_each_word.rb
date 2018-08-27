5d59284e2f3602f863d05d68f8c61a92716676fe
William Lathrop <wlathrop10@gmail.com>
2018-08-27 20:05:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
-  words_array = sentence.split(" ")
+  word_array = sentence.split(" ")
   new_array = []
-  new_array = words_array.collect {|word| word.reverse }
+  new_array = word_array.collect {|word| word.reverse }
   new_array.join(" ")
 end
